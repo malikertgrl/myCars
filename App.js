bf77e3f24cd32184a13aef58f46c86fdb979110f
malikertgrl <79869051+malikertgrl@users.noreply.github.com>
2021-09-17 18:53:14 +0300

    App.js functional componente dönüştürüldü

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
@@ -11,56 +11,64 @@ import codePush from "react-native-code-push";
 //merge denemesi 
 let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_START };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isConnected: true
-    };
+const App = () => {
 
 
-    this.unsubscribeNetInfo = NetInfo.addEventListener((info) => {
-      console.log("netinfo 2", info)
-      // this.setState({ isConnected: info.isConnected })
-      // if (!info.isConnected) {
-      //   alert(
-      //     "internet yok!"
-      //   );
-      // }
+
+  const unsubscribeNetInfo = NetInfo.addEventListener((info) => {
+    console.log("netinfo type: ", info.type)
+    console.log("netinfo isConnected: ", info.isConnected)
+
+    // this.setState({ isConnected: info.isConnected })
+    // if (!info.isConnected) {
+    //   alert(
+    //     "internet yok!"
+    //   );
+    // }
+  });
+  unsubscribeNetInfo();
+
+  useEffect(() => {
+    NetInfo.fetch().then(state => {
+      if (!state.isConnected) {
+        console.log("internte", state.isConnected)
+        alert(`internet bağlantınızı kontrol ediniz!, isConnected: ${state.isConnected}`)
+        // <View style={{ backgroundColor: "red", width: "100%", padding: 10 }}>
+        //   <Text style={{ alignSelf: "center", color: "white", fontSize: 15 }}>İnternet bağlantısı yok</Text>
+        // </View>
+
+      }
     })
-  }
+  }, [])
 
-  componentWillUnmount() {
-    this.unsubscribeNetInfo();
-  }
 
-  componentDidMount() {
+
+
+
+  useEffect(() => {
     codePush.sync({
       updateDialog: true,
       installMode: codePush.InstallMode.IMMEDIATE
     });
-  }
+  }, [])
 
 
 
 
 
-  render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
-    if (!this.state.isConnected) {
-      return (
-        <View style={{ backgroundColor: "red", width: "100%", padding: 10 }}>
-          <Text style={{ alignSelf: "center", color: "white", fontSize: 15 }}>İnternet bağlantısı yok</Text>
-        </View>
-      )
-    } else {
-      return (
-        <Provider store={store}>
-          <Router />
-        </Provider>
-      )
 
-    }
-  }
+
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
+  return (
+
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  )
+
 }
+
+
+
 export default codePush(codePushOptions)(App);
